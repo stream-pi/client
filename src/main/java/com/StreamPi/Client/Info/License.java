2d0162d4a2f0860439e33b07d01a88316119e6a0
rnayabed <debayansutradhar3@gmail.com>
2021-01-31 23:38:17 +0530

    Documentation - 1

--- src/main/java/com/StreamPi/Client/Info/License.java
+++ src/main/java/com/StreamPi/Client/Info/License.java
@@ -1,9 +1,25 @@
+/*
+Stream-Pi - Free & Open-Source Modular Cross-Platform Programmable Macropad
+Copyright (C) 2019-2021  Debayan Sutradhar (rnayabed),  Samuel Quiñones (SamuelQuinones)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Written by : Debayan Sutradhar (rnayabed)
+*/
+
 package com.StreamPi.Client.Info;
 
 public class License {
     public static String getLicense()
     {
-        return "Stream-Pi - Free & Opensource Modular Cross-Platform Programmable Macropad\n" +
+        return "Stream-Pi - Free & Open-Source Modular Cross-Platform Programmable Macro Pad\n" +
             "Copyright (C) 2019-2021  Debayan Sutradhar (rnayabed),  Samuel Quiñones (SamuelQuinones)\n" +
             "\n" +
             "This program is free software: you can redistribute it and/or modify\n" +
