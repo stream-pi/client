2d885449e415d9274a7b751b93475abb97fb42a9
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-06-29 17:44:00 +0530

    Show full screen toggle button by default

--- src/main/java/com/stream_pi/client/info/StartupFlags.java
+++ src/main/java/com/stream_pi/client/info/StartupFlags.java
@@ -7,6 +7,6 @@ public class StartupFlags
     public static boolean IS_X_MODE = false;
     public static boolean SCREEN_SAVER_FEATURE= false;
     public static boolean DEFAULT_FULLSCREEN_MODE=false;
-    public static boolean SHOW_FULLSCREEN_TOGGLE_BUTTON=false;
+    public static boolean SHOW_FULLSCREEN_TOGGLE_BUTTON=true;
     public static boolean APPEND_PATH_BEFORE_RUNNER_FILE_TO_OVERCOME_JPACKAGE_LIMITATION = false;
 }
