124591cc6a81dd96cff15615985066e9edb20ca3
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-10-22 22:13:18 +0530

    rebuild

--- src/main/java/com/stream_pi/client/Main.java
+++ src/main/java/com/stream_pi/client/Main.java
@@ -8,9 +8,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-
+public class Main extends Application
+{
     @Override
     public void start(Stage stage)
     {
