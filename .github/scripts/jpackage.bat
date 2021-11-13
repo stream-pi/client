6b3a7a48e07bbbc048396724a3e0e92ecd562035
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-11-14 01:47:30 +0530

    normalise git line endings, add command to jpackage.bat

--- .github/scripts/jpackage.bat
+++ .github/scripts/jpackage.bat
@@ -1,3 +1,17 @@
+::
+:: Stream-Pi - Free & Open-Source Modular Cross-Platform Programmable Macro Pad
+:: Copyright (C) 2019-2021  Debayan Sutradhar (rnayabed),  Samuel Quiñones (SamuelQuinones)
+::
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+::
+
 set /p REQ_MODULES=<req_modules.txt
 set /p VERSION=<version.txt
 
