c8b978f40e505e8401d41d5fe2beadcd0e5ba75e
Debayan Sutradhar <debayansutradhar3@gmail.com>
2022-01-03 22:56:47 +0530

    remove static resource bundle init

--- src/main/java/com/stream_pi/client/i18n/I18N.java
+++ src/main/java/com/stream_pi/client/i18n/I18N.java
@@ -31,8 +31,8 @@ import java.util.stream.Stream;
 public class I18N
 {
     public static Locale BASE_LOCALE = new Locale("en");
-    private static ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(I18N.class.getPackageName()+".lang");
-    private static ResourceBundle BASE_RESOURCE_BUNDLE = null;
+    private static ResourceBundle RESOURCE_BUNDLE;
+    private static ResourceBundle BASE_RESOURCE_BUNDLE;
 
     public static void init(Locale locale)
     {
